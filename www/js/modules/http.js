d4112ec22ccb87da7d229c396e44b283a5070f05
Bruno Serrão <brunosserrao@gmail.com>
2016-05-18 02:52:57 -0300

    Acessing API

--- www/js/modules/http.js
+++ www/js/modules/http.js
@@ -1,34 +1,40 @@
 angular.module('http.services', [])
 
-.factory('RequestService', function($http, $timeout, $ionicLoading){
+.factory('RequestService', function($http, $timeout, $ionicLoading, $translateLocalStorage, PopupService){
     var timeout = 10000;
 
-    $ionicLoading.show();
-
-    var urlApi = 'https://demo.wp-api.org/wp-json/wp/v2';
 
     function request(method,endPoint, data, callback) {
-        $http({
-            method: method, 
-            data: data, 
-            url: urlApi + '/' + endPoint,
-            timeout: timeout
-        }).then(
-            function successCallback(response){
-                if (callback) {
-                    callback(response.data)
+        var lang = $translateLocalStorage.get('NG_TRANSLATE_LANG_KEY');
+        var urlApi = 'https://pluris2016.fundepes.br/wp-json/wp/v2/' + endPoint + '?lang=' + lang;
+
+        $ionicLoading.show({
+             showBackdrop: false
+        });
+        
+        // if(window.Connection) {
+            $http({
+                method: method, 
+                data: data, 
+                url: urlApi,
+                timeout: timeout
+            }).then(
+                function successCallback(response){
+                    if (callback) {
+                        callback(response.data)
+                    }
+                },
+                function errorCallback(response){
+                   PopupService.alert(response.status);
                 }
-            },
-            function errorCallback(response){
-                alert('erro ' + response.status);
-            }
-        ).finally(
-            function acabei(){
-                $timeout(function () {
-                    $ionicLoading.hide();
-                }, 2000);
-            }
-        )
+            ).finally(
+                function acabei(){
+                    $timeout(function () {
+                        $ionicLoading.hide();
+                    }, 2000);
+                }
+            )
+        // }
     }
 
     return {
