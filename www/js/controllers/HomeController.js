13b670c6440c308c59e978c4b22e289bbf4cd0c4
Bruno Serrão <brunosserrao@gmail.com>
2016-05-18 02:52:18 -0300

    renaming variable homeNews

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,6 +1,8 @@
 angular.module('starter.controllers')
 .controller('HomeController', ['$scope','RequestService', function($scope, RequestService){
 	RequestService.request('get','posts',null,function(data){
-		$scope.homeNews = data;
+		if (data) {
+			$scope.noticias = data;
+		}
 	});
 }]);
