3d3220372a59ef283a01bd6763af97001fe9f05f
Bruno Serrão <brunosserrao@gmail.com>
2016-05-24 16:50:22 -0300

    Apresentação - dynamic content

--- www/js/controllers/CongressoController.js
+++ www/js/controllers/CongressoController.js
@@ -1,3 +1,11 @@
-angular.module('starter.controllers').controller('CongressoController', ['$scope', function($scope){
-	// console.log('aqui');
+angular.module('starter.controllers').controller('CongressoController', ['$scope','RequestService', function($scope, RequestService, $cordovaSocialSharing){
+	RequestService.request('GET','/pagina/2',null,function(result){
+		if (result) {
+			$scope.apresentacao = result.data;
+		}
+	});
+
+	$scope.shareAnywhere = function() {
+        $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+    }
 }]);
