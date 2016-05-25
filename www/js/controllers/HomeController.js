bf7aeccfe2e729d705dacf59c8591cd8858d596b
Bruno Serrão <brunosserrao@gmail.com>
2016-05-25 03:03:09 -0300

    Apresentação / Home content dynamic

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,9 +1,9 @@
 angular.module('starter.controllers')
-.controller('HomeController', ['$scope','RequestService', function($scope, RequestService){
+.controller('HomeController', function($scope, RequestService){
 	RequestService.request('GET','/home',null,function(result){
 		if (result) {
 			$scope.apresentacao = result.data.apresentacao.data;
 			$scope.noticias = result.data.noticias.data;
 		}
 	});
-}]);
+});
