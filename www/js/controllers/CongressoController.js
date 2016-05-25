f2aabd36499a2f89ec248a2be7691227203d0251
Bruno Serrão <brunosserrao@gmail.com>
2016-05-25 03:04:14 -0300

    Moved temporary share  test code

--- www/js/controllers/CongressoController.js
+++ www/js/controllers/CongressoController.js
@@ -1,11 +1,7 @@
-angular.module('starter.controllers').controller('CongressoController', ['$scope','RequestService', function($scope, RequestService, $cordovaSocialSharing){
+angular.module('starter.controllers').controller('CongressoController', function($scope, $translate, $stateParams, RequestService){
 	RequestService.request('GET','/pagina/2',null,function(result){
 		if (result) {
 			$scope.apresentacao = result.data;
 		}
 	});
-
-	$scope.shareAnywhere = function() {
-        $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
-    }
-}]);
+});
