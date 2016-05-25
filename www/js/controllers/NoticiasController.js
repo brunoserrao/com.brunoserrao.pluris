ffac3383ce45071685bf7b2c3b623a41d9d627c3
Bruno Serrão <brunosserrao@gmail.com>
2016-05-25 03:02:20 -0300

    Noticias Controller and Service

--- www/js/controllers/NoticiasController.js
+++ www/js/controllers/NoticiasController.js
@@ -1,8 +1,28 @@
 angular.module('starter.controllers')
-.controller('NoticiasController', ['$scope','$stateParams','RequestService', function($scope, $stateParams, RequestService){
-	RequestService.request('GET','/noticias/' + $stateParams.id,null,function(result){
-		if (result) {
-			$scope.noticia = result.data[0];
-		}
-	});
-}]);
+.controller('NoticiasController', function($scope, $timeout, $ionicScrollDelegate, NoticiasService){
+	paged = 1;
+	$scope.items_disponiveis = true;
+	$scope.noticias = [];
+
+	$scope.carregar_noticias = function(){
+		NoticiasService.noticias(paged,function(result){
+
+			for (var i = 0; i < result.data.length; i++) {
+				$scope.noticias.push(result.data[i]);
+			}
+			// $scope.noticias.push(result.data);
+			$scope.paging = result.paging;
+
+			if ( result.paging.actual_page == result.paging.total_pages  ) {
+				$scope.items_disponiveis = false;
+			}
+
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+			paged++;
+		});
+	}
+
+	// NoticiasService.noticia(84,function(result){
+	// 	console.log(result);
+	// });
+});
---
+++ www/js/services/noticias/noticias.js
@@ -0,0 +1,25 @@
+angular.module('noticias.services', [])
+
+.factory('NoticiasService', function(RequestService){
+    
+    function noticias(paged,callback) {
+       RequestService.request('GET','/noticias/?paged=' + paged,null,function(result){
+            if (result) {
+                callback(result)
+            }
+        });
+    }
+
+    function noticia(id, callback) {
+        RequestService.request('GET','/noticias/' + id,null,function(result){
+            if (result) {
+                callback(result);
+            }
+        });
+    }
+
+    return {
+        noticias : noticias,
+        noticia : noticia
+    }
+});
