e4e1d8ff63fca11f1cde8a5fc148af4c44f7ec73
Bruno Serrão <brunosserrao@gmail.com>
2016-05-26 13:50:06 -0300

    removing controller

--- www/js/controllers/CongressoController.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.controllers').controller('CongressoController', function($scope, $translate, $stateParams, RequestService){
-	RequestService.request('GET','/pagina/2',null,function(result){
-		if (result) {
-			$scope.apresentacao = result.data;
-		}
-	});
-});
