dee309c7dfb862073247a15d1b1baa7d2dd07014
Bruno Serrão <brunosserrao@gmail.com>
2016-05-30 18:28:27 -0300

    Home - get data from storage

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,9 +1,15 @@
 angular.module('starter.controllers')
-.controller('HomeController', function($scope, RequestService){
+.controller('HomeController', function($scope, RequestService, StorageService){
 	RequestService.request('GET','/home',null,function(result){
 		if (result) {
 			$scope.pagina = result.data.pagina.data;
 			$scope.noticias = result.data.noticias.data;
+			
+			StorageService.set('apresentacao-home',result.data.pagina.data);
+			StorageService.set('noticias-home',result.data.noticias.data);
+		} else {
+			$scope.pagina = StorageService.get('apresentacao-home');
+			$scope.noticias = StorageService.get('noticias-home');
 		}
 	});
 });
