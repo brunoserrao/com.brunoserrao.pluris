74b97c78d77dbef986e94bb1e73f53204bb0c05c
Bruno Serrão <brunosserrao@gmail.com>
2016-05-30 19:20:45 -0300

    Store noticias in localStorage

--- www/js/controllers/NoticiasController.js
+++ www/js/controllers/NoticiasController.js
@@ -1,24 +1,31 @@
 angular.module('starter.controllers')
-.controller('NoticiasController', function($scope, NoticiasService){
+.controller('NoticiasController', function($scope, NoticiasService, StorageService){
 	paged = 1;
 	$scope.items_disponiveis = true;
 	$scope.noticias = [];
 
-	$scope.carregar_noticias = function(){
+	$scope.carregarNoticias = function(){
 		NoticiasService.noticias(paged,function(result){
+			if (result) {
+				for (var i = 0; i < result.data.length; i++) {
+					$scope.noticias.push(result.data[i]);
+					StorageService.set('noticias',$scope.noticias);
+					StorageService.set('noticias-paging',$scope.noticias);
+				}
 
-			for (var i = 0; i < result.data.length; i++) {
-				$scope.noticias.push(result.data[i]);
-			}
+				$scope.paging = result.paging;
 
-			$scope.paging = result.paging;
+				if ( result.paging.actual_page == result.paging.total_pages  ) {
+					$scope.items_disponiveis = false;
+				}
 
-			if ( result.paging.actual_page == result.paging.total_pages  ) {
+				$scope.$broadcast('scroll.infiniteScrollComplete');
+				paged++;
+			} else {
+				$scope.noticias = StorageService.get('noticias');
+				$scope.paging = StorageService.get('noticias-paging');
 				$scope.items_disponiveis = false;
 			}
-
-			$scope.$broadcast('scroll.infiniteScrollComplete');
-			paged++;
 		});
 	}
 });
