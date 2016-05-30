96494e6f74dbe6bd7a47d547f0eab0be62f1fe22
Bruno Serrão <brunosserrao@gmail.com>
2016-05-30 20:03:15 -0300

    Store paginas in localStorage

--- www/js/controllers/PaginasController.js
+++ www/js/controllers/PaginasController.js
@@ -1,5 +1,10 @@
-angular.module('starter.controllers').controller('PaginasController', function($scope, $stateParams, PaginasService){
+angular.module('starter.controllers').controller('PaginasController', function($scope, $stateParams, PaginasService, StorageService){
 	PaginasService.pagina($stateParams.id,function(result){
-		$scope.pagina = result.data;
+		if (result) {
+			$scope.pagina = result.data;
+			StorageService.set('pagina-' + $stateParams.id, $scope.pagina);
+		} else {
+			$scope.pagina = StorageService.get('pagina-' + $stateParams.id);
+		}
 	});
 });
