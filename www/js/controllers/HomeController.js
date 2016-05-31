23cee4d582b555df8f171b15fd61ae741c745dff
Bruno Serrão <brunosserrao@gmail.com>
2016-05-31 03:12:51 -0300

    Pull to update

--- www/js/controllers/HomeController.js
+++ www/js/controllers/HomeController.js
@@ -1,15 +1,20 @@
 angular.module('starter.controllers')
 .controller('HomeController', function($scope, RequestService, StorageService){
-	RequestService.request('GET','/home',null,function(result){
-		if (result) {
-			$scope.pagina = result.data.pagina.data;
-			$scope.noticias = result.data.noticias.data;
-			
-			StorageService.set('apresentacao-home',result.data.pagina.data);
-			StorageService.set('noticias-home',result.data.noticias.data);
-		} else {
-			$scope.pagina = StorageService.get('apresentacao-home');
-			$scope.noticias = StorageService.get('noticias-home');
-		}
-	});
+	
+	$scope.atualizarHome = function() {
+		RequestService.request('GET','/home',null,function(result){
+			if (result) {
+				$scope.pagina = result.data.pagina.data;
+				$scope.noticias = result.data.noticias.data;
+				
+				StorageService.set('apresentacao-home',result.data.pagina.data);
+				StorageService.set('noticias-home',result.data.noticias.data);
+			} else {
+				$scope.pagina = StorageService.get('apresentacao-home');
+				$scope.noticias = StorageService.get('noticias-home');
+			}
+
+			 $scope.$broadcast('scroll.refreshComplete');
+		});
+	};
 });
