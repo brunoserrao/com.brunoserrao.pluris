1250d3cc35cc75c501b426c0c69958b4fedf9db5
Bruno Serrão <brunosserrao@gmail.com>
2016-05-31 02:36:03 -0300

    Menu and submenu

--- www/js/app.js
+++ www/js/app.js
@@ -1,42 +1,30 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'pascalprecht.translate', 'ngCookies','ngCordova'])
 
 .run(function ($ionicPlatform) {
 	$ionicPlatform.ready(function () {
-		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		// for form inputs)
 		if (window.cordova && window.cordova.plugins.Keyboard) {
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 			cordova.plugins.Keyboard.disableScroll(true);
 
 		}
+
 		if (window.StatusBar) {
-			// org.apache.cordova.statusbar required
 			StatusBar.styleDefault();
 		}
 
-		// hide splashscreen
 		if (navigator.splashscreen) {
 			setTimeout(function() {
 				navigator.splashscreen.hide();
 			}, 100);
 		}
 
+		// TODO: Notifications actions
 		if (typeof ionic.Platform.device().available !== 'undefined') {
-			// Enable to debug issues.
-			// window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
 			var notificationOpenedCallback = function(jsonData) {
 				console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
 			};
 
 			window.plugins.OneSignal.init("b69e6a23-9185-43f4-9746-17886415f661",{googleProjectNumber: "749121535538"},notificationOpenedCallback);
-			// Show an alert box if a notification comes in when the user is in your app.
-			// window.plugins.OneSignal.enableInAppAlertNotification(true);
 		}
 	});
 })
