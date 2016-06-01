e0c0c89c9dc07e25dbab7ab55a5b43a97f4bda7d
Bruno Serrão <brunosserrao@gmail.com>
2016-05-31 21:14:27 -0300

    NoticiaService - bug fix loading

--- www/js/controllers/NoticiasViewController.js
+++ www/js/controllers/NoticiasViewController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
-.controller('NoticiasViewController', function($scope, $stateParams, NoticiasService){	
-	NoticiasService.noticia($stateParams.id,function(result){
+.controller('NoticiasViewController', function($scope, $stateParams, NoticiasService){
+	NoticiasService.noticia($stateParams.id, true,function(result){
 		$scope.noticia = result.data[0];
 	});
 });
