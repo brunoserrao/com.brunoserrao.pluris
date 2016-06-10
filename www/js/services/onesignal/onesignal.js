1c6901cc7ebf6374e82988f24ecb3b39fbea116b
Bruno Serrão <brunosserrao@gmail.com>
2016-06-10 18:41:33 -0300

    Notifications icon counter

--- www/js/services/onesignal/onesignal.js
+++ www/js/services/onesignal/onesignal.js
@@ -1,15 +1,21 @@
 angular.module('onesignal.services', [])
 
-.factory('OneSingalService', function($ionicPlatform, $state){
+.factory('OneSingalService', function($ionicPlatform, $state, $rootScope, StorageService){
 
     var init = function(){
+        $rootScope.contador = StorageService.get('contador');
+
         if (typeof ionic.Platform.device().available !== 'undefined') {
             window.plugins.OneSignal.init("b69e6a23-9185-43f4-9746-17886415f661",{googleProjectNumber: "749121535538"}, additionalData);
-            window.plugins.OneSignal.enableNotificationsWhenActive(true);
+            window.plugins.OneSignal.enableInAppAlertNotification(true);
+            window.plugins.OneSignal.enableNotificationsWhenActive(false);
         }
     }
 
     var additionalData = function(jsonData) {
+        $rootScope.contador++;
+        StorageService.set('contador',  $rootScope.contador);
+
         if (Object.keys(jsonData.additionalData).length) {
             if (!jsonData.isActive) {
                 $state.go(jsonData.additionalData.redirect,{ 'id' : jsonData.additionalData.id });
@@ -25,5 +31,34 @@ angular.module('onesignal.services', [])
         init : init,
         alert : alert,
         subscribe : subscribe
+    }
+})
+.directive('notificacaoContador', function(){
+    return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+            contador : '='
+        },
+        templateUrl: 'templates/contador/contador.html',
+        link: function (scope, elem, attrs) {
+            elem.bind('click', function(){
+                scope.alertar();
+            });
+        },
+        controller: function($scope,$rootScope, $timeout, $cordovaNativeAudio, StorageService){
+            $scope.alertar = function($scope){
+                alert('mostrar popover');
+                
+                if (typeof ionic.Platform.device().available !== 'undefined') {
+                    $cordovaNativeAudio.play('bongo');
+                }
+
+                $timeout(function(){
+                    $rootScope.contador = 0;
+                    StorageService.set('contador',  $rootScope.contador);
+                },100);
+            }
+        }
     }
 });
--- www/js/services/paginas/paginas.js
+++ www/js/services/paginas/paginas.js
@@ -3,7 +3,7 @@ angular.module('paginas.services', [])
 .factory('PaginasService', function(RequestService){
     
     function pagina(id, loading, callback) {
-        RequestService.request('GET','/pagina/' + id, loading, true, function(result){
+        RequestService.request('GET','/pagina/' + id, false, loading, function(result){
             callback(result);
         });
     }
