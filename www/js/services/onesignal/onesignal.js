3f83d2c93afb0ae4f7a41a2806158e621afeee70
Bruno Serrão <brunosserrao@gmail.com>
2016-06-13 21:15:53 -0300

    Notifications - Popover and counter

--- www/js/services/onesignal/onesignal.js
+++ www/js/services/onesignal/onesignal.js
@@ -3,18 +3,31 @@ angular.module('onesignal.services', [])
 .factory('OneSingalService', function($ionicPlatform, $state, $rootScope, StorageService){
 
     var init = function(){
-        $rootScope.contador = StorageService.get('contador');
+        notificacoes = StorageService.get('notificacoes');
 
+        if (!notificacoes) {
+            notificacoes = [];
+            StorageService.set('notificacoes', notificacoes);
+        }
+        
         if (typeof ionic.Platform.device().available !== 'undefined') {
             window.plugins.OneSignal.init("b69e6a23-9185-43f4-9746-17886415f661",{googleProjectNumber: "749121535538"}, additionalData);
             window.plugins.OneSignal.enableInAppAlertNotification(true);
             window.plugins.OneSignal.enableNotificationsWhenActive(false);
         }
+
+        // for (var id_teste = 0; id_teste < 10; id_teste++) {
+        //     jsonData = {"message": id_teste + " - Mensagem em Português","additionalData":{"redirect":"app.programacao/id","id": id_teste,"title":"Titulo em Português 13/06/2016 16:53:39"},"isActive":false};
+        //     additionalData(jsonData);   
+        // }
     }
 
     var additionalData = function(jsonData) {
-        $rootScope.contador++;
-        StorageService.set('contador',  $rootScope.contador);
+        notificacoes = StorageService.get('notificacoes');
+        notificacoes.reverse();
+        notificacoes.push(jsonData);
+        StorageService.set('notificacoes',  notificacoes);
+        $rootScope.contador = notificacoes.length;
 
         if (Object.keys(jsonData.additionalData).length) {
             if (!jsonData.isActive) {
@@ -29,7 +42,6 @@ angular.module('onesignal.services', [])
 
     return {
         init : init,
-        alert : alert,
         subscribe : subscribe
     }
 })
@@ -41,23 +53,34 @@ angular.module('onesignal.services', [])
             contador : '='
         },
         templateUrl: 'templates/contador/contador.html',
-        link: function (scope, elem, attrs) {
-            elem.bind('click', function(){
-                scope.alertar();
+        controller: function($scope, $state, $ionicListDelegate, $rootScope, $ionicPopover, $timeout, StorageService){
+
+            $scope.notificacoes = StorageService.get('notificacoes');
+            $scope.contador = $scope.notificacoes.length;
+
+            $ionicPopover.fromTemplateUrl('templates/popover/notificacoes.html', {
+                scope: $scope
+            }).then(function(popover) {
+                $scope.popover = popover;
             });
-        },
-        controller: function($scope,$rootScope, $timeout, $cordovaNativeAudio, StorageService){
-            $scope.alertar = function($scope){
-                alert('mostrar popover');
-                
-                if (typeof ionic.Platform.device().available !== 'undefined') {
-                    $cordovaNativeAudio.play('bongo');
-                }
+
+            $scope.openPopover = function($event) {
+                $scope.popover.show($event);
+            };
+
+            $scope.clickNotificacao = function(notificacao, index){
+                $state.go(notificacao.additionalData.redirect,{ 'id' : notificacao.additionalData.id });
+                $scope.popover.hide();
+                $scope.deletarNotificacao(index);
+            }
+
+            $scope.deletarNotificacao = function(index, notificacao){
+                $scope.notificacoes.splice(index,1);
+                $scope.contador = $scope.notificacoes.length;
 
                 $timeout(function(){
-                    $rootScope.contador = 0;
-                    StorageService.set('contador',  $rootScope.contador);
-                },100);
+                    StorageService.set('notificacoes', $scope.notificacoes);
+                },100)
             }
         }
     }
